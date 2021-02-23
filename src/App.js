859ac094a2dd7f247ba091945db9fa3eb55f1175
Racine Felipe <felipe.xdsam@gmail.com>
2021-02-23 19:58:01 -0300

    IU atualizada

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
+import Todo from './components/Todo';
+import Date from './components/Date';
+
+const App = () => (
+  <div className="container flex-center">
+    <Date />
+
+    <div className="todos-container flex-center">
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
     </div>
-  )
-}
 
-export default App
+    <div>
+      <button type="button" className="add-btn">ADD</button>
+    </div>
+  </div>
+);
 
+export default App;
---
+++ src/components/Date/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const index = () => (
+  <div className="date-container">
+    <div className="date-dmy">
+      <div className="date-day">16</div>
+      <div className="date-month-year">
+        <div className="date-month">Agosto</div>
+        <div className="date-year">2016</div>
+      </div>
+    </div>
+    <div className="date-day-name">Segunda-feira</div>
+  </div>
+);
+
+export default index;
