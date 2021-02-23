c3ddc562b0921f89e5511caf99d188691f1b2cd0
Racine Felipe <felipe.xdsam@gmail.com>
2021-02-23 20:35:51 -0300

    Date component atualizada com biblioteca moment

--- src/components/Date/index.jsx
+++ src/components/Date/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
-const index = () => (
-  <div className="date-container">
-    <div className="date-dmy">
-      <div className="date-day">16</div>
-      <div className="date-month-year">
-        <div className="date-month">Agosto</div>
-        <div className="date-year">2016</div>
+import moment from 'moment';
+import 'moment/locale/pt-br';
+
+const index = () => {
+  console.log(moment().locale());
+
+  return (
+    <div className="date-container">
+      <div className="date-dmy">
+        <div className="date-day">{moment().format('DD')}</div>
+        <div className="date-month-year">
+          <div className="date-month">{moment().format('MMMM')}</div>
+          <div className="date-year">{moment().format('YYYY')}</div>
+        </div>
       </div>
+      <div className="date-day-name">{moment().format('dddd')}</div>
     </div>
-    <div className="date-day-name">Segunda-feira</div>
-  </div>
-);
+  );
+};
 
 export default index;
